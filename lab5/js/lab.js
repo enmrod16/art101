50ace1846846f944d4efd7c2fa21c985411d9660
Saint <tropix309@gmail.com>
2024-10-20 03:48:31 -0700

    LAB5
    uploading local files. possibly debugging and testing to reupload later.

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * index.js - Lab5
+ * Author: Saint Rodriguez
+ * Created: 20 October 2024
+*/
 
-// Constants
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
 
-// Functions
+// Calculate
+age = 2019 - year
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Output 
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();

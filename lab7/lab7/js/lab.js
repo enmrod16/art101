7c88ac04304a9637d691b469fcf782c64dcab46f
Saint <tropix309@gmail.com>
2024-10-27 21:34:15 -0700

    Lab 7 and Task 2/Task 3
    Both tasks are done, tested, debugged, and now being uploaded.

--- lab7/lab7/js/lab.js
+++ lab7/lab7/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * index.js - Lab7
+ * Author: Saint Rodriguez
+ * Created: 28 October 2024
+*/
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+//sortUserName is a function that takes user input and sorts the letters of their name
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //splits string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sorts the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //joins array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("I've fixed your name: ", sortUserName(), "</br>");

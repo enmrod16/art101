0dbcff33d91ffe1aa139bab1a57df5842c515f99
Saint <tropix309@gmail.com>
2024-10-30 18:25:45 -0700

    Lab 8
    no bonus task.

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,6 +1,35 @@
 /*
- * index.js - Lab7
+ * index.js - Lab8
  * Author: Saint Rodriguez
- * Created: 28 October 2024
+ * Created: 30 October 2024
 */
 
+// variables
+var numbers = [1, 2, 25, 36];
+
+// squareroot function
+function sQuareRoot(x) {
+    var results = Math.sqrt(x);
+    return results;
+}
+
+// testing function
+console.log(sQuareRoot(16));
+console.log(sQuareRoot(4));
+
+//using map on my array (numbers) to use my sqrt function on
+//all the numbers in the array
+var squareResults = numbers.map(sQuareRoot);
+
+
+//changed operation, now subtracts -1
+//anon function + results
+var subtractResults = numbers.map(function(x) {
+    var results = x - 1;
+    return results;
+})
+ 
+//prints in console
+console.log("Array numbers: ", numbers);
+console.log("Square roots: ", squareResults);
+console.log("Results of numbers -1: ", subtractResults); 

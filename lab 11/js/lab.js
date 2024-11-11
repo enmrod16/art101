5113dd122696eb2bdb5e468d2e7a98e3dfa51ab0
Saint <tropix309@gmail.com>
2024-11-11 15:26:36 -0800

    Lab 11 small JS writing

--- lab 11/js/lab.js
+++ lab 11/js/lab.js
@@ -4,16 +4,14 @@
  * Created: 11 November 2024
 */
 
-
+// function, sorts the string in alphabetical order
 function sortString(inputString) {
     return inputString.split('').sort().join('');
 }
 
+//buttons and input field
 $("#submit").click(function(){
-
 const userName = $("#user-name").val();
-
 userNameSorted = sortString(userName);
-
 $("#output").html('<div class="text"><p>'+ userNameSorted + '</p></div>');
 });
